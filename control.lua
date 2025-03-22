ea372c636c85cef3e26130184df0274f9dffae98
Distortions81 <carlotto81@gmail.com>
2025-03-22 17:02:33 -0600

    version

--- control.lua
+++ control.lua
@@ -18,7 +18,7 @@ require "quickbar" -- Save or Restore Quickbar
 require "stash" -- Save or Restore Weapon/Ammo/Armor
 
 function RunSetup()
-    storage.SM_Version = "639-01.31.2025-0326"
+    storage.SM_Version = "640-03.22.2025-0502p"
 
     storage.SM_OldVersion = storage.SM_Version
 
