109ebe3accc5f98e673c431c979862202c54983c
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2025-06-09 18:11:44 -0600

    Convert score to string before localization

--- info.lua
+++ info.lua
@@ -507,7 +507,7 @@ function INFO_InfoWin(player)
                 caption = {
                     "",
                     "[color=orange][font=default-large-bold]",
-                    {"strings.info_score_current", math.floor(storage.PData[player.index].score / 60 / 60)},
+                    {"strings.info_score_current", tostring(math.floor(storage.PData[player.index].score / 60 / 60))},
                     "[/font][/color]"
                 }
             }
