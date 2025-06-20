4c418a5d75ffccacfb3085ba8ae396b57d4083b3
Distortions81 <carlotto81@gmail.com>
2025-06-20 16:06:48 -0600

    version

--- control.lua
+++ control.lua
@@ -24,7 +24,7 @@ script.on_init(function()
   end)
 
 function RunSetup()
-    storage.SM_Version = "652-06.20.2025-0100p"
+    storage.SM_Version = "653-06.20.2025-0406"
 
     if not storage.SM_OldVersion then
         storage.SM_OldVersion = "OldVersion"
